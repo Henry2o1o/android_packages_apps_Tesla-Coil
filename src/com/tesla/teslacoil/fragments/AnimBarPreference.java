5dbb688bdef7722c5be73601348e7b690ac2eaba
theringmaster <gzroms@hotmail.com>
2015-11-26 08:32:02 -0500

    Animation consolidation
    Poet and i didn't know it ;)
    Credits to DU for the references

--- src/com/tesla/teslacoil/fragments/AnimBarPreference.java
+++ src/com/tesla/teslacoil/fragments/AnimBarPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.tesla.teslacoil.fragments.animation;
+package com.tesla.teslacoil.fragments;
 
 import android.content.Context;
 import android.content.res.TypedArray;
